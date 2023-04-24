7366891665d18862951373238cb33ed8e1003b46
monokemonoke <kawabataryuhei@gmail.com>
2023-04-24 20:04:25 +0900

    pdf binary printer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{env::args, fs::File, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    assert_eq!(args.len(), 2, "no args");
+    let filename = &args[1];
+
+    let mut file = File::open(filename).expect("file not found");
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).unwrap();
+
+    let cs: Vec<String> = buf
+        .iter()
+        .map(|v| {
+            if let Ok(c) = std::str::from_utf8(&[*v]) {
+                c.to_string()
+            } else {
+                format!("<{}>", v)
+            }
+        })
+        .collect();
+    println!("{}", cs.join(""));
 }
